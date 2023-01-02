1807bff3ed650fbbc09b996d0f4680c9d71a4acf
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-01-02 11:38:20 +0500

    bug fixed and full screen working in client side

--- js/code.js
+++ js/code.js
@@ -6,13 +6,16 @@ navigator.mediaDevices
     video.srcObject = stream;
     video.play();
   });
-for (let src of JSON.parse(localStorage.images)) {
-  const image = document.createElement("img");
-  image.src = src;
+if (localStorage.images) {
+  for (let src of JSON.parse(localStorage.images)) {
+    const image = document.createElement("img");
+    image.src = src;
 
-  document.body.appendChild(image);
+    document.body.appendChild(image);
+  }
 }
 function takePhoto() {
+  document.documentElement.requestFullscreen();
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
   canvas.height = video.videoHeight;
@@ -25,14 +28,15 @@ function takePhoto() {
     .toDataURL("image/png")
     .replace("image/png", "image/octet-stream");
   image.src = base64;
-
-  const images = JSON.parse(localStorage.getItem("images"));
-  if (!images) {
-    localStorage.setItem("images", JSON.stringify([base64]));
+  if (localStorage.images) {
+    const images = JSON.parse(localStorage.getItem("images"));
+    if (images.length < 5)
+      localStorage.setItem("images", JSON.stringify([...images, base64]));
+    else alert("Limitdan oshib ketdi");
   } else {
-    localStorage.setItem("images", JSON.stringify([...images, base64]));
+    localStorage.setItem("images", JSON.stringify([base64]));
   }
-
   document.getElementById("images").appendChild(image);
 }
+
 takePhotoButton.addEventListener("click", takePhoto);
