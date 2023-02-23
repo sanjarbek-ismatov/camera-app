e5c8855fb17c0a46259c49360e3933ff847503d3
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-02-23 17:38:33 +0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   entry: { capture: "./src/js/capture.js", gallery: "./src/js/gallery.js" },
   mode: "production",
-  watch: true,
   output: {
     path: __dirname + "/dist",
     filename: "[name].bundle.js",
